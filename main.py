460a704c4722e4500b7c6ec2f626e2729ca178ee
5412x <72889112+5412x@users.noreply.github.com>
2020-11-18 21:52:55 +0100

    Added checksum to file details

--- main.py
+++ main.py
@@ -2,20 +2,23 @@
 
 __author__ = "pyd4nt1c"
 __version__ = 1.0
+__license__ = "MIT License"
 
 
 # -- Imports --
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, File, UploadFile
 from fastapi.responses import FileResponse, HTMLResponse, PlainTextResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from typing import Optional
 from pydantic import BaseModel
+import sys
 import os.path
 from requests import post
-import mimetypes
 import time
+import mimetypes
 import json
+import hashlib
 # -------------
 
 
@@ -31,7 +34,7 @@ app.mount(
     "/static", StaticFiles(directory="static"), name="static")
 
 # -- Constants --
-BASE_URL = "https://p4wn.eu/"
+BASE_URL = "https://dl.p4wn.eu/"
 
 SITE_KEY = "6Lezv-MZAAAAAHTFigO2zL-lJPgejkyDYDM-I_dt"
 SECRET_KEY = "****************************************"
@@ -45,6 +48,8 @@ UNITS_MAPPING = [
     (1 << 10, ' KB'),
     (1, (' byte', ' bytes')),
 ]
+
+BUF_SIZE = 65536  # 64kb chunks
 # --------------
 
 
@@ -89,6 +94,7 @@ def get_icon(file_type):
     full_file_type = file_type
     file_type = file_type.split("/")
     file_type = file_type[1]
+
     if file_type == "javascript":
         icon = "fab fa-js"
     elif file_type == "msword":
@@ -103,9 +109,11 @@ def get_icon(file_type):
         icon = "fas fa-file-powerpoint"
     elif file_type == "vnd.ms-excel":
         icon = "fas fa-file-excel"
-    elif file_type == {"x-tar", "zip"}:
+    elif file_type == {"x-tar", "zip", "x-freearc"}:
         icon = "fas fa-archive"
-    elif file_type in {"mp4", "x-msvideo", "quicktime"} or full_file_type == "video/mpeg":
+    elif file_type in {"x-bzip", "x-bzip2"}:
+        icon = "fas fa-file-archive"
+    elif file_type in {"mp4", "x-msvideo", "quicktime"} or full_file_type in {"video/mpeg", "video/ogg"}:
         icon = "fas fa-video"
     elif file_type == "css":
         icon = "fab fa-css3"
@@ -115,12 +123,16 @@ def get_icon(file_type):
         icon = "fab fa-python"
     elif file_type in {"plain", "json"}:
         icon = "fas fa-file-alt"
-    elif file_type in {"xml", "x-sh"}:
+    elif file_type in {"xml", "x-sh", "x-csh"}:
         icon = "fas fa-code"
     elif file_type in {"jpeg", "gif", "bmp", "png", "svg+xml"}:
         icon = "fas fa-image"
-    elif file_type == "x-wav" or full_file_type in {"audio/mpeg", "audio/basic"}:
+    elif file_type == "x-wav" or full_file_type in {"audio/mpeg", "audio/basic", "audio/aac", "audio/ogg"}:
         icon = "fas fa-volume-up"
+    elif file_type == "x-debian-package":
+        icon = "fas fa-box"
+    elif file_type == "java-archive":
+        icon = "fab fa-java"
     else:
         icon = "fas fa-file"
     return icon
@@ -153,25 +165,58 @@ def get_creation_date(file):
         return creation_date
     except:
         return "Unknown"
+
+
+def md5sum(file):
+    md5 = hashlib.md5()
+    with open("files/" + file, 'rb') as data_stream:
+        while True:
+            data = data_stream.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+
+    return md5.hexdigest()
 # ---------------
 
 
 # -- Paths --
+
+# -- Root --
 @app.get("/")
-def get_root():
+async def get_root():
     return PlainTextResponse("No")
 
 
 @app.get("/favicon.ico")
-def get_favicon():
+async def get_favicon():
     return FileResponse("favicon.ico")
 
 
+@app.get("/license")
+async def get_license(request: Request):
+    return templates.TemplateResponse("license.html", {
+        "request": request,
+    })
+# ----------
+
+
+# -- Uploading files --
+# @app.post("/upload")
+# async def upload_file(file: UploadFile = File()):
+#    return "a"
+# ---------------------
+
+
+# -- Downloading and viewing files --
 @app.get("/{file}")
-def file_details(request: Request, file: str):
+async def file_details(request: Request, file: str):
     # Check if the file exists
     if not os.path.isfile("files/" + file):
-        raise HTTPException(status_code=404, detail="File not found")
+        return templates.TemplateResponse("file_not_found.html", {
+            "file_name": file,
+            "request": request
+        })
 
     file_type = get_type(file)
     icon = get_icon(file_type)
@@ -182,6 +227,10 @@ def file_details(request: Request, file: str):
         file_type = file_type[0]
     file_size = get_size(file)
     creation_date = get_creation_date(file)
+    try:
+        integrity = md5sum(file)
+    except:
+        integrity = "Error"
 
     return templates.TemplateResponse("file_details.html", {
         "request": request,
@@ -192,12 +241,13 @@ def file_details(request: Request, file: str):
         "file_type": file_type,
         "file_encoding": file_encoding,
         "file_size": file_size,
-        "creation_date": creation_date
+        "creation_date": creation_date,
+        "md5sum": integrity
     })
 
 
 @app.get("/download/{file}")
-def download_file(file: str, captcha_token: str):
+async def download_file(file: str, captcha_token: str):
     if len(file) > 100:  # Check if the file name isn't too long
         raise HTTPException(status_code=400, detail="File name too long")
     # Check if the file exists
@@ -212,9 +262,29 @@ def download_file(file: str, captcha_token: str):
     return FileResponse("files/" + file)
 
 
-@app.get("/edit/{file}")
-def view_file(file: str):
+@app.get("/view/{file}")
+async def view_file(request: Request, file: str, captcha_token: str):
     if len(file) > 100:  # Check if the file name isn't too long
         raise HTTPException(status_code=400, detail="File name too long")
+
+    # Check if the file exists
+    if not os.path.isfile("files/" + file):
+        raise HTTPException(status_code=404, detail="File not found")
+
+    # Verify captcha
+    if not verify_captcha(captcha_token):
+        raise HTTPException(status_code=403, detail="Captcha token invalid")
+
+    # If everything matches up
+    file_content = open("files/" + file, "r")
+    file_content = file_content.read()
+
+    return templates.TemplateResponse("view_file.html", {
+        "request": request,
+        "file_name": file,
+        "file_content": file_content
+    })
+# -----------------------------------
+
 
 # -----------

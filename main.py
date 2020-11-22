e50a1ada4793e949035c3e076de389b8980c4bb8
5412x <72889112+5412x@users.noreply.github.com>
2020-11-22 18:09:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ templates = Jinja2Templates(directory="templates")
 BASE_URL = "https://p4wn.eu/"
 
 SITE_KEY = "6Lezv-MZAAAAAHTFigO2zL-lJPgejkyDYDM-I_dt"
-SECRET_KEY = "6Lezv-MZAAAAAOc8RINviafOLS01oIIEeZ7vUusc"
+SECRET_KEY = "****************************************"
 
 # Defined units
 UNITS_MAPPING = [
